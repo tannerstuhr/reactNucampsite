80375c3adc2ae62f13d55a5a000980c1b7e4d1e6
tannerstuhr <tannerjstuhr@gmail.com>
2022-04-06 00:14:14 -0700

    Components Part 2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'typeface-lobster';
+import 'typeface-open-sans';
 
 ReactDOM.render(
   <React.StrictMode>
