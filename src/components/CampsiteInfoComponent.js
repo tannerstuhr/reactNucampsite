0c5158cc32596e813cc75af3502da703e2ad883a
tannerstuhr <tannerjstuhr@gmail.com>
2022-04-13 23:38:13 -0700

    Presentational and Container Components

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,61 @@
+import { render } from "@testing-library/react";
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class CampsiteInfo extends Component {
+
+    renderCampsite(campsite) {
+        return (
+            <div className="col-md-5 m-1">
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        )
+    }
+
+    renderComments(comments) {
+        if (comments) {
+            return (
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {comments.map((comment) => {
+                        return (
+                            <div key={comment.id}>
+                                <p>
+                                    {comment.text}
+                                    <br />
+                                    -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                                </p>
+                            </div>
+                        )
+                    })}
+                </div>
+            )
+        }
+        return <div></div>
+    }
+
+    render(campsite) {
+        if (this.props.campsite) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
+                </div>
+            )
+        }
+        return(
+            <div></div>
+        )
+    };
+}
+
+
+export default CampsiteInfo
--- src/shared/campsites.js
+++ src/shared/campsites.js
@@ -53,37 +53,37 @@ export const CAMPSITES = [
             {
                 id: 0,
                 rating: 5, 
-                text: "What a magnificent view!",
-                author: "Tinus Lorvaldes",
-                date: "2018-10-25T16:30Z"
+                text: "The phishing was incredible!",
+                author: "Olivia Maguire",
+                date: "2018-11-14T16:30Z"
             },
             {
                 id: 1,
                 rating: 4, 
-                text: "The campground was beautiful, but the bugs could bite sometimes.",
-                author: "Brennen Ech",
-                date: "2017-06-17T03:33Z"
+                text: "Renting a raft is a must here! Super fun!",
+                author: "Marlon Bishop",
+                date: "2018-06-27T03:33Z"
             },
             {
                 id: 2,
                 rating: 5, 
-                text: "I caught a world-record-breaking trout here on my last visit.",
-                author: "McKenzie Sebastian",
-                date: "2019-02-18T18:12Z"
+                text: "You could see the whole milky way at night.",
+                author: "Shamus Fulton",
+                date: "2020-04-12T18:12Z"
             },
             {
                 id: 3,
                 rating: 5, 
-                text: "A wonderful place to reconnect with nature.",
-                author: "Jordan Runn",
-                date: "2019-08-04T20:11Z"
+                text: "There was a rope swing by our campsite which was a blast!",
+                author: "Lilly Abbott",
+                date: "2017-02-19T20:11Z"
             },
             {
                 id: 4,
                 rating: 4, 
-                text: "The stars at night were magnificent!",
-                author: "Ann Dabramov",
-                date: "2018-07-23T19:44Z"
+                text: "Falling asleep to the sound of the river was the best!",
+                author: "Jessica Holden",
+                date: "2020-09-06T19:44Z"
             }
         ]
     },
@@ -97,37 +97,37 @@ export const CAMPSITES = [
             {
                 id: 0,
                 rating: 5, 
-                text: "What a magnificent view!",
-                author: "Tinus Lorvaldes",
-                date: "2018-10-25T16:30Z"
+                text: "Very peaceful at night.",
+                author: "Amalie Bonilla",
+                date: "2021-12-13T16:30Z"
             },
             {
                 id: 1,
                 rating: 4, 
-                text: "The campground was beautiful, but the bugs could bite sometimes.",
-                author: "Brennen Ech",
-                date: "2017-06-17T03:33Z"
+                text: "The rangers gave us free firewood!",
+                author: "Abdul Taylor",
+                date: "2018-05-22T03:33Z"
             },
             {
                 id: 2,
                 rating: 5, 
-                text: "I caught a world-record-breaking trout here on my last visit.",
-                author: "McKenzie Sebastian",
-                date: "2019-02-18T18:12Z"
+                text: "These trees make for some great hammock spots!",
+                author: "Kaiden Knapp",
+                date: "2020-09-26T18:12Z"
             },
             {
                 id: 3,
                 rating: 5, 
-                text: "A wonderful place to reconnect with nature.",
-                author: "Jordan Runn",
-                date: "2019-08-04T20:11Z"
+                text: "The bugs here are huge! The campsites had a lot of space.",
+                author: "Domini Wickens",
+                date: "2018-07-05T20:11Z"
             },
             {
                 id: 4,
                 rating: 4, 
-                text: "The stars at night were magnificent!",
-                author: "Ann Dabramov",
-                date: "2018-07-23T19:44Z"
+                text: "These campsites were great, plenty of spots to pitch some tents",
+                author: "Adrianna Hart",
+                date: "2019-09-28T19:44Z"
             }
         ]
     },
@@ -141,37 +141,37 @@ export const CAMPSITES = [
             {
                 id: 0,
                 rating: 5, 
-                text: "What a magnificent view!",
-                author: "Tinus Lorvaldes",
-                date: "2018-10-25T16:30Z"
+                text: "Wow these trees are gorgeous!",
+                author: "Eren Crawford",
+                date: "2018-04-14T16:30Z"
             },
             {
                 id: 1,
                 rating: 4, 
-                text: "The campground was beautiful, but the bugs could bite sometimes.",
-                author: "Brennen Ech",
-                date: "2017-06-17T03:33Z"
+                text: "Great spot but the sites are a little close.",
+                author: "Craig Ball",
+                date: "2018-06-20T03:33Z"
             },
             {
                 id: 2,
                 rating: 5, 
-                text: "I caught a world-record-breaking trout here on my last visit.",
-                author: "McKenzie Sebastian",
-                date: "2019-02-18T18:12Z"
+                text: "The lake was a perfect temperature for swimming!",
+                author: "Antonio Costa",
+                date: "2021-07-04T18:12Z"
             },
             {
                 id: 3,
                 rating: 5, 
-                text: "A wonderful place to reconnect with nature.",
-                author: "Jordan Runn",
-                date: "2019-08-04T20:11Z"
+                text: "There are tons of great hikes here!",
+                author: "Conrad Bouvet",
+                date: "2020-03-12T20:11Z"
             },
             {
                 id: 4,
                 rating: 4, 
-                text: "The stars at night were magnificent!",
-                author: "Ann Dabramov",
-                date: "2018-07-23T19:44Z"
+                text: "Fun place but bring your bug spray!",
+                author: "Joanne Mclaughlin",
+                date: "2018-08-13T19:44Z"
             }
         ]
     }
