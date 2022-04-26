76d3fb16e5246afbf42ba6dbe3f22d0ba74d1e0f
tannerstuhr <tannerjstuhr@gmail.com>
2022-04-26 00:29:21 -0700

    Redux Thunk

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,7 @@ import CampsiteInfo from '../components/CampsiteInfoComponent';
 import Header from '../components/HeaderComponent';
 import Directory from '../components/DirectoryComponent';
 import Footer from '../components/FooterComponent';
-import { addComment } from '../redux/ActionCreators';
+import { addComment, fetchCampsites } from '../redux/ActionCreators';
 
 
 
@@ -22,17 +22,24 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = {
-  addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text))
+  addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
+  fetchCampsites: () => (fetchCampsites())
 };
 
 class Main extends Component {
+
+    componentDidMount() {
+      this.props.fetchCampsites();
+    }
 
     render() {
 
         const HomePage = () => {
             return (
               <Home
-                campsite={this.props.campsites.filter(campsite => campsite.featured)[0]}
+                campsite={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
+                campsitesLoading={this.props.campsites.isLoading}
+                campsitesErrMess={this.props.campsites.errMess}
                 promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
                 partner={this.props.partners.filter(partner => partner.featured)[0]}
               />
@@ -43,10 +50,12 @@ class Main extends Component {
             return (
               <CampsiteInfo
                 campsite={
-                  this.props.campsites.filter(
+                  this.props.campsites.campsites.filter(
                     campsite => campsite.id === +match.params.campsiteId
                   )[0]
                 }
+                isLoading={this.props.campsites.isLoading}
+                errMess={this.props.campsites.errMess}
                 comments={this.props.comments.filter(
                   comment => comment.campsiteId === +match.params.campsiteId
                 )}
