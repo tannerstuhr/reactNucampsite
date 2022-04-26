8248702c8cd7c6d07af9906a8286ba039c311f17
tannerstuhr <tannerjstuhr@gmail.com>
2022-04-26 00:49:58 -0700

    React Redux Forms Revisited

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -8,7 +8,7 @@ import {
   Row
 } from 'reactstrap';
 import {Link} from 'react-router-dom';
-import { Control, LocalForm, Errors} from 'react-redux-form';
+import { Control, Form, actions, Errors} from 'react-redux-form';
 
 const required = val => val && val.length; 
 const maxLength = len => val => !val || (val.length <= len);
@@ -44,6 +44,7 @@ class Contact extends Component {
   handleSubmit(values) {
     console.log('Current state is: ' + JSON.stringify(values));
     alert('Current state is: ' + JSON.stringify(values));
+    this.props.resetFeedbackForm();
   }
   render() {
 
@@ -90,7 +91,7 @@ class Contact extends Component {
             <hr />
           </div>
           <div className="col-md-10">
-            <LocalForm onSubmit={values => this.handleSubmit(values)}>
+            <Form model="feedbackForm" onSubmit={values => this.handleSubmit(values)}>
               <Row className="form-group">
                 <Label htmlFor="firstName" md={2}>
                   First Name
@@ -248,7 +249,7 @@ class Contact extends Component {
                   </Button>
                 </Col>
               </Row>
-            </LocalForm>
+            </Form>
           </div>
         </div>
       </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,6 +8,7 @@ import CampsiteInfo from '../components/CampsiteInfoComponent';
 import Header from '../components/HeaderComponent';
 import Directory from '../components/DirectoryComponent';
 import Footer from '../components/FooterComponent';
+import { actions } from 'react-redux-form';
 import { addComment, fetchCampsites } from '../redux/ActionCreators';
 
 
@@ -23,7 +24,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = {
   addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
-  fetchCampsites: () => (fetchCampsites())
+  fetchCampsites: () => (fetchCampsites()),
+  resetFeedbackForm: () => (actions.reset('feedbackForm'))
 };
 
 class Main extends Component {
@@ -75,7 +77,8 @@ class Main extends Component {
                 render={() => <Directory campsites={this.props.campsites} />}
               />
               <Route path="/directory/:campsiteId" component={CampsiteWithId} />
-              <Route exact path="/contactus" component={Contact} />
+              <Route exact path="/contactus" render={() => <Contact resetFeedbackForm=
+              {this.props.resetFeedbackForm} />} />
               <Route
                 exact
                 path="/aboutus"
